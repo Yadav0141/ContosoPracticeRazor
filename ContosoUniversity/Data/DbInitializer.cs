212e29643fd10d14babbfe20de1fd883be0e2d21
Abhishek Kumar <abhishekyadav589@gmail.com>
2018-03-08 06:59:35 +0530

    ef migrations

--- ContosoUniversity/Data/DbInitializer.cs
+++ ContosoUniversity/Data/DbInitializer.cs
@@ -7,7 +7,7 @@ namespace ContosoUniversity.Data
     {
         public static void Initialize(SchoolContext context)
         {
-            context.Database.EnsureCreated();
+           
             // Look for any students.
             if (context.Students.Any())
             {
